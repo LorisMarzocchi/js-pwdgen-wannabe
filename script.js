44cc4dc75bc4d752cefb59f59b299fb1880b1bb5
LorisMarzocchi <ml77182a@gmail.com>
2023-04-04 20:15:03 +0200

    fixtest

--- script.js
+++ script.js
@@ -2,20 +2,49 @@
 
 const userName = prompt('come ti chiami?');
 
-const userLasyName = prompt(`qual'è il tuo cognome?`);
+const userLastName = prompt(`qual'è il tuo cognome?`);
 
 const userColor = prompt(`qual'è il tuo colore preferito?`);
 
-let year = 23;
-year = parseInt(year);
+const year = 23;
 
-let sumPass = userName + userLasyName + userColor + year;
-
-// let sumPass = userName + userLasyName + userColor + `23`;
+const sumPass = userName + userLastName + userColor + year;
 
 document.getElementById('password').innerHTML = sumPass;
 document.getElementById("User_name").innerHTML = userName;
 
 
-let passUser = `la tua password è: ${sumPass}`;
+const passUser = `la tua password è: ${sumPass}`;
 console.log(passUser);
+
+
+
+
+
+// input e controllo numero
+
+// const userName = prompt('come ti chiami?');
+
+// const userLasyName = prompt(`qual'è il tuo cognome?`);
+
+// const userColor = prompt(`qual'è il tuo colore preferito?`);
+
+// const anno = parseInt(prompt(`qual'è il tuo anno di nascita?`));
+
+// let year = anno;
+// if (isNaN(anno)) {
+//   year = '#';
+  
+// } else {
+//   year = anno;
+
+// }
+// const sumPass = userName + userLasyName + userColor + year;
+
+
+// document.getElementById('password').innerHTML = sumPass;
+// document.getElementById("User_name").innerHTML = userName;
+
+
+// const passUser = `la tua password è: ${sumPass}`;
+// console.log(passUser);
