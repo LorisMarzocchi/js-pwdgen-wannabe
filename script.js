1cde3ea0a5d0c6ac26cc4988f5b7916771be0869
LorisMarzocchi <ml77182a@gmail.com>
2023-04-04 15:17:42 +0200

    newtest

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ const userLasyName = prompt(`qual'è il tuo cognome?`);
 
 const userColor = prompt(`qual'è il tuo colore preferito?`);
 
-const sumPass = userName + userLasyName + userColor +`23`;
+let year = 23;
+year = parseInt(year);
+
+let sumPass = userName + userLasyName + userColor + year;
+
+// let sumPass = userName + userLasyName + userColor + `23`;
 
 document.getElementById('password').innerHTML = sumPass;
 document.getElementById("User_name").innerHTML = userName;
 
-console.log(sumPass);
+
+let passUser = `la tua password è: ${sumPass}`;
+console.log(passUser);
